687ec0e458ba5af27506901df173ae462220373f
CakeVision <andreicodorean1811@gmail.com>
2024-09-18 15:51:30 +0300

    basic flake

---
+++ hosts/common/users/cake.nix
@@ -0,0 +1,29 @@
+{ config
+, pkgs
+, inputs
+, ...
+}: {
+  users.users.cake = {
+    initialHashedPassword = "$y$j9T$46H6fi69q2KhnLFLUQ1BG1$Ji4yE8MRrr6iPnFCGkXfqYrJYCr9raTrJr11Ya9NOpA";
+    isNormalUser = true;
+    description = "Cake user";
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "audio"
+      "video"
+      "flatpak"
+      "plugdev"
+      "input"
+      "libvirt"
+      "kvm"
+      "qemu-libvirt"
+    ];
+    packages = [ inputs.home-manager.packages.${pkgs.system}.default ];
+
+  };
+
+  home-manager.users.cake =
+    import cake/${config.networking.hostName}.nix;
+  fonts.fontconfig.enable = true;
+}
