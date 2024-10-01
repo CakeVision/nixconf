29bd065cd57169fd90b8c67726c22369f84fbcc5
CakeVision <andreicodorean1811@gmail.com>
2024-10-01 16:41:18 +0300

    installed droid_studio, haproxy and vscode(i am sorry for my sins, but I aint got time to adapt the lsp for nvim...)

--- home/cake/home.nix
+++ home/cake/home.nix
@@ -39,6 +39,8 @@
       brave
       nerdfonts
       android-studio
+      haproxy
+      vscode
 
       # # Adds the 'hello' command to your environment. It prints a friendly
       # # "Hello, world!" when run.
